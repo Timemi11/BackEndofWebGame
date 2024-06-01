42976ed1b1c0ef35b605ce096b50b3f2fb99c0cb
time <chatchawan.aromtip@gmail.com>
2024-06-01 23:25:03 +0700

    ss

--- src/index.ts
+++ src/index.ts
@@ -242,9 +242,11 @@ app.post("/sent-gameproduct/:userId", async (req: Request, res: Response) => {
   }else{
     prod_price = "ลดเหลือ " +(prod_price/100).toFixed(0)+ ' บาท' ;
   }
-  if(prod_beforeprice === 0 || prod_beforeprice === null || prod_beforeprice === prod_price){
+  if(prod_beforeprice === 0 || prod_beforeprice === null ){
     prod_beforeprice = '-'
-  }else{
+    }else if(prod_beforeprice === prod_price){
+      prod_beforeprice = '-'
+    }else{
     prod_beforeprice = 'จาก '+ (prod_beforeprice/100).toFixed(0)+ ' บาท' ;
   }
  
