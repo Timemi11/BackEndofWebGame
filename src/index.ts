5158c80149be83e180adaf81de2fe0df08a19e9e
time <chatchawan.aromtip@gmail.com>
2024-06-01 20:36:28 +0700

    12

--- src/index.ts
+++ src/index.ts
@@ -234,7 +234,7 @@ app.post("/sent-gameproduct/:userId", async (req: Request, res: Response) => {
   // console.log(req.body)
   console.log(req.body)
   console.log("userId=> "+userId)
-  const {prod_id, prod_img, prod_name, prod_desc, prod_price,url,steamurl } = req.body ;
+  const {prod_id, prod_img, prod_name, prod_desc, prod_origiprice,prod_finalprice,url,steamurl } = req.body ;
 
  
    client.pushMessage({
@@ -345,7 +345,7 @@ app.post("/sent-gameproduct/:userId", async (req: Request, res: Response) => {
                           "contents": [
                             {
                               "type": "text",
-                              "text": "จาก "+ (prod_price + (prod_price*50/100)).toFixed(0)+" บาท",
+                              "text": "จาก "+ prod_origiprice.toFixed(0)+" บาท",
                               "style": "italic",
                               "size": "sm",
                               "decoration": "line-through",
@@ -356,7 +356,7 @@ app.post("/sent-gameproduct/:userId", async (req: Request, res: Response) => {
                         },
                         {
                           "type": "text",
-                          "text": "ลดเหลือ "+ prod_price.toFixed(0)+" บาท",
+                          "text": "ลดเหลือ "+ prod_finalprice.toFixed(0)+" บาท",
                           "color": "#22c55e",
                           "size": "md",
                           "style": "normal",
